1b74822fcc254da631020219dce307f1ce8d1670
ZW181818 <203756310@qq.com>
2026-08-05 16:48:30 -0700

    fix: bundle web icon font

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,8 +1,16 @@
+import { useFonts } from 'expo-font';
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [fontsLoaded, fontError] = useFonts({
+    MaterialSymbols_400Regular: require('../../assets/fonts/MaterialSymbols_400Regular.ttf'),
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
